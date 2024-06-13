b4cc256dd2cd953fb86a4da787e5b28a567a4fdf
DaniloRNakasawa <danilonakasawa@outlook.com>
2024-06-13 19:44:48 -0300

    feat req Inventory Report

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,33 @@
+from inventory_report.inventory import Inventory
+from datetime import datetime
+
+
 class SimpleReport:
-    pass
+    def __init__(self) -> None:
+        self.inventories = []
+
+    def add_inventory(self, inventory: Inventory) -> None:
+        self.inventories.append(inventory)
+
+
+def generate(self) -> str:
+    oldest_manufacturing_date = min(
+        product.manufacturing_date
+        for inventory in self.inventories
+        for product in inventory.data
+    )
+    closest_expiration_date = min(
+        product.expiration_date
+        for inventory in self.inventories
+        for product in inventory.data
+        if product.expiration_date > datetime.now()
+    )
+    company_with_largest_inventory = max(
+        self.inventories, key=lambda inventory: len(inventory.data)
+    ).company
+
+    return (
+        f"Oldest manufacturing date: {oldest_manufacturing_date}\n"
+        f"Closest expiration date: {closest_expiration_date}\n"
+        f"Company with the largest inventory: {company_with_largest_inventory}"
+    )
